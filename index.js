1d2be9ec6a0c2b5e1c3a46d2023120477a1827b9
FizraFaheem <160629677+FizraFaheem@users.noreply.github.com>
2024-03-27 09:43:36 +0500

    Simple_Calculator

---
+++ index.js
@@ -0,0 +1,29 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+//printing a welcome message
+console.log("\n\tWellcome To \'QiratFaheem\' -CLI Simple Calculator\n");
+// Asking Question from user through inquirer
+let answer = await inquirer.prompt([
+    { message: "Enter First Number", type: "number", name: "firstNumber" },
+    { message: "Enter Second Number", type: "number", name: "SecondNumber" },
+    {
+        message: "Select one Operator to perform Operation",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Substraction", "Multiplication", "Devision"]
+    },
+]);
+console.log(answer);
+// condition statement if/else
+if (answer.operator === "Addition") {
+    console.log(answer.firstNumber + answer.SecondNumber);
+}
+else if (answer.operator === "Substraction") {
+    console.log(answer.firstNumber - answer.SecondNumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstNumber * answer.SecondNumber);
+}
+else if (answer.operator === "Devision") {
+    console.log(answer.firstNumber / answer.SecondNumber);
+}
